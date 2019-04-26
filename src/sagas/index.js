4d5d907ae7e61f262fccd945f9a8da830dba31d5
slee2540 <slee2540@gmail.com>
2019-04-26 17:08:44 +0900

    사가 수정

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -5,12 +5,10 @@ function* fetchNews() {
   const json = yield fetch(
     "https://newsapi.org/v1/articles?source=cnn&apiKey=c39a26d9c12f48dba2a5c00e35684ecc"
   ).then(response => response.json());
-  // console.log(1111);
   yield put({ type: type.REQUEST_API_SUCCESS, json: json.articles });
 }
 
 function* actionWatcher() {
-  // console.log(2222);
   yield takeLatest(type.REQUEST_API, fetchNews);
 }
 
